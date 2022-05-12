ff1e0e30b33f9f7737e7a4151f136d7640a34695
eaglu <chcw38@gmail.com>
2022-05-12 19:32:20 +0800

    Refactor _check_events, depart key press and key release event into two method, press q to quit game,ship's movement now based on speed

--- settings.py
+++ settings.py
@@ -2,6 +2,10 @@ class Settings:
 
     def __init__(self) -> None:
         """Initialize the game's settings."""
+        # Screen size settings
         self.screen_width = 1200
         self.screen_height = 800
+        # Background color
         self.bg_color = (230, 230, 230)
+        # Ship speed settings
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -5,12 +5,10 @@ class Ship:
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
-        """"Initialize the ship and set its starting position."""
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
-
-        # Load the ship image and get its rect.
         self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -20,15 +18,21 @@ class Ship:
         self.rect.midbottom = self.screen_rect.midbottom
 
         # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Moving flags
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Update the ship's position by moving flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        """Update the ship's position by moving flag,speed based on ship_speed from setting,limited by screen."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
+
+        # Update the ship's x value based on self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
