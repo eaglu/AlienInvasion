d4cc3d96f3b143675adfa67068d50b9747660c73
eaglu <chcw38@gmail.com>
2022-05-12 18:48:30 +0800

    Create Settings and Ship,fix typo,add notes,format file

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,13 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
+
 class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
+
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -16,7 +25,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             pygame.display.flip()
+
 
 if __name__ == "__main__":
     a = AlienInvasion()
