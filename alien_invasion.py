e5b167d761c646ddd96fd21d50a5420e96e185a9
eaglu <chcw38@gmail.com>
2022-05-21 11:14:53 +0800

    Create alien group

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,10 +92,32 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-    def _create_fleet(self):
-        """Create alien group."""
+    def _create_alien(self, alien_number, row_number):
         alien = Alien(self)
+        alien_width, alien_hight = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _create_fleet(self):
+        # Create an alien and calculate the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Calculate the number of rows of aliens that the screen can fit.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (3 * alien_height)
+
+        # Create aliens group
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
 
 
 if __name__ == "__main__":
